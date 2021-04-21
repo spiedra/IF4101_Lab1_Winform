96ec5ba796dd9db4aa64733f29aa57fa5f63481e
spiedra <jcspiedra@gmail.com>
2021-04-21 12:38:48 -0600

    SQL scripts added

---
+++ SQL/INSERT INTO [COUNTRIES].[tb_COUNTRY].sql
@@ -0,0 +1,34 @@
+USE IF4101_LAB1_B97452
+GO
+
+INSERT INTO [COUNTRIES].[tb_COUNTRY]
+(
+  [CURRENCY_ID]
+, [COUNTRY_NAME]
+)
+VALUES
+(
+ 1
+,'Costa Rica'
+),(
+ 2
+,'Mexico'
+),(
+3
+,'Colombia'
+),(
+4
+,'Spain'
+),(
+5
+,'Canada'
+),(
+6
+,'China'
+),(
+7
+,'United Kingdom'
+),(
+8
+,'Japan'
+)
---
+++ SQL/INSERT INTO [CURRENCY].[tb_CURRENCY].sql
@@ -0,0 +1,43 @@
+USE IF4101_LAB1_B97452
+GO
+
+INSERT INTO [CURRENCY].[tb_CURRENCY]
+(
+   [CURRENCY_NAME]
+ , [DOLLAR_VALUE]
+)
+VALUES
+(
+ 'Costa Rican Colon'
+ ,613.86627
+),(
+ 'Mexican peso'
+ ,19.87544
+),(
+ 'Colombian peso'
+ ,3643.45282
+),(
+ 'Euro'
+ ,0.83245
+),(
+'Canadian dollars'
+, 1.2487365
+),(
+'Chinese yuan renminbi'
+, 6.4903229
+),(
+'British pound'
+, 0.71790297
+),(
+'Japanese Yen'
+, 108.16577
+),(
+'Guatemalan Quetzal'
+, 7.7123927
+),(
+'Indian Rupees'
+, 75.441314
+),(
+'U.S. dollars'
+, 1.00000
+)
